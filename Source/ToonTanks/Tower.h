dc7e5ac7beeb87b757adc06a47c586eb1b30853b
thecrasx <75530512+TheCrasx@users.noreply.github.com>
2023-03-12 16:02:22 +0100

    Tower Init

---
+++ Source/ToonTanks/Tower.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATower : public ABasePawn
+{
+	GENERATED_BODY()
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+};
