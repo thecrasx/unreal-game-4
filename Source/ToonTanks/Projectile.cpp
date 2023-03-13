c2357e99dc7e9d65787e9d7b78099b69970a51e4
thecrasx <75530512+TheCrasx@users.noreply.github.com>
2023-03-13 22:28:33 +0100

    Projectile Init

---
+++ Source/ToonTanks/Projectile.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Projectile.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Sets default values
+AProjectile::AProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	ProjectileMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile Mesh"));
+
+	RootComponent = ProjectileMesh;
+
+	ProjectileMovementComp = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement Component"));
+	ProjectileMovementComp->MaxSpeed = 1300;
+	ProjectileMovementComp->InitialSpeed = 1300;
+
+}
+
+// Called when the game starts or when spawned
+void AProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
